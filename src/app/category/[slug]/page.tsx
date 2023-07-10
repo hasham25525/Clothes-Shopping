fec20b2a402ad88d34ecd19bf91203f98e313c44
Muhammad Hasham <hasham25525@gmail.com>
2023-07-10 18:45:45 +0500

    nav

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -2,10 +2,24 @@ import React from 'react'
 import { Products } from '@/utils/mock'
 import ProductCard from '@/components/ProductCard'
 
-const getProductByCategory=(category:string)=>{
-    Products.filter((product)=>product.category===category)
+const getProductByCategory = (category: string) => {
+    Products.filter((product) => product.category === category)
 }
 
 export default function Page({ params }: { params: { slug: string } }) {
-    return <div>My Category: {params.slug}</div>
-  } 
+    const result = getProductByCategory(params.slug);
+    return(
+        <div className='flex justify-evenly py-10  flex-wrap'>
+        {
+            result.map((product) => (
+                <ProductCard
+                    key={product.id}
+                    title={product.name}
+                    price={product.price}
+                    img={product.image}
+                    category={product.category} />
+            ))
+        }
+    </div> 
+    )
+} 
