b54c23f932ce1029026c92ad747e84f97ebad402
Muhammad Hasham <hasham25525@gmail.com>
2023-07-12 02:43:49 +0500

    quantity edited

--- src/components/Quantity.tsx
+++ src/components/Quantity.tsx
@@ -1,19 +1,19 @@
 "use client"
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 const Quantity = () => {
     const [num, setNum] = useState(1);
     return (
         <>
             <div className='flex ml-3 mt-3 gap-4'>
-                <button className='w-6 h-6 border rounded-full flex justify-center items-center hover:' onClick={()=>{
-                    setNum(num-1);
+                <button className='w-6 h-6 border rounded-full flex justify-center items-center hover:' onClick={() => {
+                    setNum(num < 2 ? 1 : num - 1);
                 }}>
                     -
                 </button>
-               <span>{num}</span>
-                <button className='w-6 h-6 border rounded-full flex justify-center items-center' onClick={()=>{
-                    setNum(num+1);
+                <span>{num}</span>
+                <button className='w-6 h-6 border rounded-full flex justify-center items-center' onClick={() => {
+                    setNum(num + 1);
                 }}>
                     +
                 </button>
