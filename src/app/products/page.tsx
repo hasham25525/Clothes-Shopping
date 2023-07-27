9b65a45d0aba52b289b03d95f94c38f46f58e2d0
Muhammad Hasham <hasham25525@gmail.com>
2023-07-28 00:56:33 +0500

    UI/UX changes

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
 
 
   return (
-    <div className='flex justify-evenly py-10 flex-wrap'>
+    <div className='flex justify-evenly py-10 flex-wrap '>
       {
         Products.map((product) => (
           <ProductCard key={product.id} title={product.name} price={product.price} img={product.image} category={product.category} id={product.id} />
