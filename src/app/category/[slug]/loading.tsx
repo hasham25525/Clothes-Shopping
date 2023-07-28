216668ae3f2fb22ce3866a1679bec2b5e4865042
Muhammad Hasham <hasham25525@gmail.com>
2023-07-29 02:28:59 +0500

    loading skeleton demo

---
+++ src/app/category/[slug]/loading.tsx
@@ -0,0 +1,7 @@
+import Skeleton from 'react-loading-skeleton'
+
+const Loading = () => {
+    // You can add any UI inside Loading, including a    Skeleton.
+    return <Skeleton />
+    }
+    export default Loading;
