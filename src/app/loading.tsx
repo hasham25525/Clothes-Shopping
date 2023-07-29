89650090a02affdb067f927cdbfb39ee0496dad8
Muhammad Hasham <hasham25525@gmail.com>
2023-07-30 01:07:20 +0500

    minor changes

--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-import Skeleton from 'react-loading-skeleton'
-
-const Loading = () => {
-    // You can add any UI inside Loading, including a    Skeleton.
-    return <Skeleton />
-    }
-    export default Loading;
